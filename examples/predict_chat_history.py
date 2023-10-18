153aef66491e041442d73ba5179992e48d07d934
ldwang <ftgreat@gmail.com>
2023-10-18 09:37:40 +0800

    Add predict_chat_history.
    Signed-off-by: ldwang <ftgreat@gmail.com>

---
+++ examples/predict_chat_history.py
@@ -0,0 +1,19 @@
+from flagai.auto_model.auto_loader import AutoLoader
+
+model_name = 'AquilaChat2-34B'
+
+autoloader = AutoLoader("aquila2", model_name=model_name)
+
+model = autoloader.get_model()
+tokenizer = autoloader.get_tokenizer()
+
+test_data = [
+    "怎样才能在有限的时间内学习新技能？",
+    "我正在学习编程，有没有一些针对初学者的在线教程推荐？",
+    "我想学习Python编程语言，哪个在线教育平台提供的课程最适合我？",
+]
+
+history = []
+for text in test_data:
+    print(f"=============================================================================")
+    print(model.predict(text, tokenizer=tokenizer, model_name=model_name, history=history))
